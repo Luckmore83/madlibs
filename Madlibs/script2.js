d5ed76b5274a8c5b078473dcbded06a9f51be9e2
daniel_a <daaksve@gmail.com>
2024-09-22 17:22:14 +0200

    Add files via upload

---
+++ Madlibs/script2.js
@@ -0,0 +1,21 @@
+function yourFunction() {
+
+var number1 = prompt("En siffra");
+var adjective1 = prompt("Ett adjektiv som slutar på a");
+var location1 = prompt("Plats i bestämd form");
+var city = prompt("En stad");
+var adjective3 = prompt("Ännu ett adjektiv");
+var adjective4 = prompt("Ett till adjektiv");
+var country = prompt("Ett land");
+var movie = prompt("En film");
+var name = prompt("En persons namn");
+var adjective5 = prompt("Ett adjektiv som slutar på a igen");
+var fmember = prompt("En släktmedlem, (ex. kusin)");
+var verb1 = prompt("Ett verb");
+var noun = prompt("Ett substantiv");
+var location2 = prompt("En plats till i bestämd form");
+var verb2 = prompt("Ett verb i dåtid");
+var adjective6 = prompt("Ett sista adjektiv");
+
+document.write("Jag hade tagit " + number1 + " veckors semester. Jag hämtade upp min " + adjective1 + " familj på " + location1 + " i " + city + ". Husvagnen var " + adjective3 + ", " + adjective4 + " och redo för resan till " + country + ". Medan barnen tittade på " + movie + ", turades jag och " + name + " , min " + adjective5 + " " + fmember + " om att köra. Barnen började " + verb1 + " om deras " + noun + " så vi fick stanna till vid " + location2 + ". De kom inte överens, vi " + verb2 + " och förberrede oss för en " + adjective6 + " resa hemåt.");
+}
